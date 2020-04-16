e97c86ea18ec44b700cc3ef349df1939eae911e0
lbugasu <lbugasu@gmail.com>
2020-04-16 11:22:55 -0400

    developing front-end pages

--- src/app.js
+++ src/app.js
@@ -42,14 +42,21 @@ app.get('/topics', function(req, res){
     
     res.render('topics.hbs');
 })
-podcast_list = [];
+podcast_list = ['"https://open.spotify.com/embed-podcast/episode/6Tnw9NvPKjkcgnTqDuV2H1"'];
 
 app.get('/topics/hope', function(req, res){
-    res.render('topic', {podcasts: podcast_list});
+    console.log(podcast_list);
+    var pod_iframes = `<iframe src=${podcast_list[0]}width="100%" height="60" frameborder="0" ></iframe>`;  
+    //document.body.innerHTML = pod_iframes;
+    res.render('topic', {podcast:pod_iframes});
 });
 app.post('/topics/hope', function(req, res){
-    podcast_list.push(req.body.podcast_url)
-
+    //podcast_list.push(req.body.podcast_url)
+    let link = /"https:\S+"/;
+    let matches = [...req.body.podcast_url.matchAll(link)];
+    //console.log("len of matches: ", matches.length);
+    let source_url = matches[0][0];
+    podcast_list.push(source_url);
     res.redirect('/topics/hope');
 })
 //people page
